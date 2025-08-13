ecb1bbef15bfaa1923d628f6d41c1ba128b086b2
Laurian Csurovski <148965580+laurycsu@users.noreply.github.com>
2025-08-13 19:20:40 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+// Cache-first PWA for ES Language Learn
 const CACHE = "es-learn-v1";
 
 const ASSETS = [
@@ -5,14 +6,12 @@ const ASSETS = [
   "./index.html",
   "./manifest.json",
   "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./sounds/correct.wav" // optional, only if used
+  "./icons/icon-512.png"
+  // Add any CSS/JS/images you reference with relative paths here if needed
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
